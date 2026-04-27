92630aeff4e21d0e4d3c8efa63205617d1eb603f
OomRr <omarabozaid358@gmail.com>
2026-04-27 12:44:34 +0300

    Initial Commit

---
+++ 2bLogin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2bLogin
+//
+//  Created by Omar on 27/04/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
